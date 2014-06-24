75cb58093d1c101ea83a9b5dff25407889e0bbe4
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2014-06-24 12:45:00 -0500

    Final code
    Have both async and non-async method for the blog post in. Read the
    final post @
    http://mkscodingblog.wordpress.com/2014/06/19/asynchronous-4/

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,33 +12,104 @@ namespace BlogStuff
     {
         static void Main(string[] args)
         {
-            Stopwatch sw = new Stopwatch(); 
+            // Async method calls and other utlity information printed out
+            // DoThingsAsync();
 
-            sw.Start();
-
-            Console.WriteLine("What numbers do you want to add 5 to. Type S to stop");
-            List<int> nums = new List<int>();
-
-            string userinput;
-            while ((userinput = Console.ReadLine()).ToLower() != "s" )
-            {
-                nums.Add(Convert.ToInt32(userinput));
-            }
-
-            nums.ForEach(num => 
-                Console.WriteLine("Adding 5 to {0} gives {1}",
-                                    num, AddFiveToNumber(num)));
-
-            sw.Stop();
-
-            Console.WriteLine("{0}ms {1}s {2}m", sw.ElapsedMilliseconds, sw.ElapsedMilliseconds/1000.0, sw.ElapsedMilliseconds/1000.0/60.0);
+            // Do things in a non-async manner 
+            // DoThingsNonAsync();
+            
 
         }
 
+        /// <summary>
+        /// This is a stub method that does nothing but clears up the cluttered code.
+        /// The main purpose of thie method is to print out the elapsed time this method took
+        /// </summary>
+        public static void DoThingsNonAsync()
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            // Console.WriteLine("What numbers do you want to add 5 to. Type S to stop");
+            List<int> nums = new List<int>();
+
+            const int MAX = 100000;
+
+            int count = 0;
+            while (count++ != MAX)
+            {
+                nums.Add(count);
+            }
+
+            int num = 0;
+            nums.ForEach(_ =>
+            {
+                num = AddFiveToNumber(_);
+            });
+
+            sw.Stop();
+
+            Console.WriteLine("Doing things in a async manner took: {0}ms {1}s {2}m",
+                sw.ElapsedMilliseconds, sw.ElapsedMilliseconds / 1000.0, sw.ElapsedMilliseconds / 1000.0 / 60.0);
+        }
+
+        /// <summary>
+        /// The main purpose of the method is to get the elapsed time the async method took.
+        /// </summary>
+        public static void DoThingsAsync()
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            // Console.WriteLine("What numbers do you want to add 5 to. Type S to stop");
+            List<int> nums = new List<int>();
+
+            const int MAX = 100000;
+
+            int count = 0;
+            while (count++ != MAX)
+            {
+                nums.Add(count);
+            }
+
+            int num = 0;
+            nums.ForEach(_ =>
+            {
+                Task<int> t = AddFiveToNumberAsync(_);
+                num = t.Result;
+            });
+
+            sw.Stop();
+
+            Console.WriteLine("Doing things in a async manner took: {0}ms {1}s {2}m",
+                sw.ElapsedMilliseconds, sw.ElapsedMilliseconds / 1000.0, sw.ElapsedMilliseconds / 1000.0 / 60.0);
+        }
+
+        /// <summary>
+        /// Add five to a number passed.
+        /// </summary>
+        /// <param name="num">What number do want to add five to?</param>
+        /// <returns>return the value of num + 5</returns>
         private static int AddFiveToNumber(int num)
         {
             return num + 5;
         }
 
+        /// <summary>
+        /// This add five to a number using the async and tasks
+        /// </summary>
+        /// <param name="num">What number to you want to add five to?</param>
+        /// <returns>return a Task of int that has already been started.</returns>
+        private static async Task<int> AddFiveToNumberAsync(int num)
+        {
+            Func<int> func = () => num+5;
+            Task<int> task = new Task<int>(func);
+            task.Start();
+
+
+            return await task;
+        }
     }
 }
